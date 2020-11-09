e053ab2d49f66fbad2cc8227bc18afb83dd0d140
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-11-09 19:30:07 +0100

    Update serial schemes

--- connio/__init__.py
+++ connio/__init__.py
@@ -4,7 +4,7 @@ __version__ = "0.1.0"
 import urllib.parse
 
 
-SERIAL_SCHEMES = {'serial', 'tcp-serial', 'rfc2217'}
+SERIAL_SCHEMES = {'serial', 'serial-tcp', 'serial-tango', 'rfc2217'}
 SOCKET_SCHEMES = {'tcp'}
 
 
@@ -17,3 +17,5 @@ def connection_for_url(url, *args, **kwargs):
     elif scheme in SOCKET_SCHEMES:
         from sockio import socket_for_url
         return socket_for_url(url, *args, **kwargs)
+    else:
+        raise ValueError("unsupported scheme {!r} for {}".format(scheme, url))
